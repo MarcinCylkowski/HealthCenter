a1ffa624d85334f85f4c75e389e8ba60ea3a0921
MarcinCylkowski <marcin.cylkowski3@gmail.com>
2020-01-07 17:43:41 +0100

    front core

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MainService } from './main.service';
+import { AppRoutingModule } from './app-router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+    
+  ],
+  providers: [MainService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
